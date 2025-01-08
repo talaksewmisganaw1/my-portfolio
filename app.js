b62b0f0e4a0618c699effa20cd82d82993e4b180
Talaksew Agegnehu Misganaw <talaksewmisganaw1@gmail.com>
2025-01-08 10:51:51 +0300

    all sections done with photos, icons and some texts to be chaged

--- app.js
+++ app.js
@@ -22,3 +22,9 @@ sectionBtns.forEach(btn => {
     })
 })
 
+//toggle theme
+const themeBtn = document.querySelector(".theme-btn");
+themeBtn.addEventListener('click', () => {
+    let element = document.body;
+    element.classList.toggle('light-mode');
+})
